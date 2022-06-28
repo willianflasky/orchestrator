c9a5a412102a3a496aed124ead487985ec3fb367
--help <284607860@qq.com>
2022-06-28 21:01:59 +0800

    test readonly 6

--- orcguard/logic/mysql.go
+++ orcguard/logic/mysql.go
@@ -1,23 +1,23 @@
 package logic
 
 import (
+	"database/sql"
 	"fmt"
 	"orcguard/models"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
 )
 
 var (
-	db       *sqlx.DB
+	db       *sql.DB
 	username = "orchestrator"
 	password = "orch_monitorpd"
-	dbname   = ""
+	dbname   = "mysql"
 )
 
 func InitDB(ip string, port int) (err error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True", username, password, ip, port, dbname)
-	db, err = sqlx.Connect("mysql", dsn)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s", username, password, ip, port, dbname)
+	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		fmt.Printf("connect oldmaster DB failed, err:%v\n", err)
 		return
@@ -28,9 +28,9 @@ func InitDB(ip string, port int) (err error) {
 }
 
 func get_readonly() (val string) {
-	sqlStr := "show variables like 'read_only'"
+	sqlStr := `show variables like 'read_only'`
 	var m models.Mysql_vars
-	err := db.Get(&m, sqlStr)
+	err := db.QueryRow(sqlStr).Scan(&m.Variable_name, &m.Value)
 	if err != nil {
 		fmt.Printf("get readonly failed, err:%v\n", err)
 		return
