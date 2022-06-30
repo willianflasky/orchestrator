6a99473c4773556f305fcdbf9f9460cd665dad6b
--help <284607860@qq.com>
2022-06-30 11:37:01 +0800

    test cmdErr topology_recovery.go

--- go/logic/topology_recovery.go
+++ go/logic/topology_recovery.go
@@ -391,6 +391,7 @@ func executeProcesses(processes []string, description string, topologyRecovery *
 			go executeProcess(command, env, topologyRecovery, fullDescription)
 		} else {
 			if cmdErr := executeProcess(command, env, topologyRecovery, fullDescription); cmdErr != nil {
+				fmt.Printf("=============: %v", cmdErr)
 				if failOnError {
 					AuditTopologyRecovery(topologyRecovery, fmt.Sprintf("Not running further %s hooks", description))
 					return cmdErr
